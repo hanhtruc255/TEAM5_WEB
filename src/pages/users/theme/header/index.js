90856050abfa07ebddcf418de4dad7c77d437c26
HANH TRUC <hanhtruc255@gmail.com>
2023-12-13 01:58:10 +0700

    hometek project

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,21 +1,100 @@
-import {memo} from "react";
+import {memo, useState} from "react";
 import './style.scss';
 import { BsHeart } from "react-icons/bs";
 import { BsCart3 } from "react-icons/bs";
 import { BsPerson } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import logo from '/HOMETEK_PJ/fe/src/assets/user/Images/LOGO-HomeTek.png';
+import React from "react";
 
 
 const Header= () =>{
+    const [Menus, setMenus] = useState ([
+        {name: "Về chúng tôi",
+         path: ""
+        },
+        {name:"Bếp",
+         path: "",
+         isShowSubmenu: false,
+         child: [
+            {name:"Tất cả sản phẩm",
+            path: "",
+            },
+            {name:"Nồi chiên không dầu",
+            path: "",
+            },
+            {name:"Nồi cơm thông minh",
+            path: "",
+            },
+            {name:"Máy sửa thực phẩm",
+            path: "",
+            },
+            {name:"Máy khử trùng đồ dùng bếp",
+            path: "",
+            }
+
+         ]
+        },
+        {name: "Dọn dẹp",
+         path: "",
+         isShowSubmenu: false,
+         child: [
+            {name:"Tất cả sản phẩm",
+            path: "",
+            },
+            {name:"Robot hút bụi lau nhà",
+            path: "",
+            },
+            {name:"Máy lọc không khí thông minh",
+            path: "",
+            },
+            {name:"Bàn chải đa năng",
+            path: "",
+            }
+         ]
+        },
+        {name: "Tiện ích",
+         path: "",
+         isShowSubmenu: false,
+         child: [
+            {name:"Tất cả sản phẩm",
+            path: "",
+            },
+            {name:"Máy tạo bọt rửa tay",
+            path: "",
+            },
+            {name:"Loa trợ lý ảo thông minh",
+            path: "",
+            },
+            {name:"Công tắc thông minh",
+            path: "",
+            }
+         ]
+        },
+        {name: "Blog",
+         path: ""
+        },
+        {name: "Chính sách",
+         path: ""
+        },
+    ])
+
     return (
-        <div className="header_top">
-            <div className="container">
+        <div>
+            <div className="header__top">
                 <div className="row">
-                    <div className="col-6">
-                        <img src="./Images/LOGO-HomeTek.png"></img>
+                    <div className="header__top__left">
+                        <Link to={"/"}> <img src={logo} alt="logo"></img></Link>
+                    </div>
+
+                    <div className="header__top__midle">
+                        <input type="text" placeholder="Bạn muốn tìm gì..."/>
+                            <Link to={""} className="iconSearch"><BsSearch /></Link>
+                        
                     </div>
  
-                    <div className="col-6 header_top_right">
+                    <div className="header__top__right">
                         <ul>
                             <li>
                                 <Link to ={""}><BsHeart /></Link>
@@ -24,19 +103,42 @@ const Header= () =>{
                                 </Link> 
                                 </li>
                             <li> <Link to ={""}><BsPerson />
-                                {/* <br><span>Đăng nhập</span>
-                                <span>Đăng ký</span></br> */}
                             </Link>
                                 </li>
                         </ul>
                     </div>
                 </div>
+            </div>
+            <div className="header__menu">
+                <nav className="Menu">
+                    <ul>
+                        {Menus?. map((menu,menukey) => (
+                                <li key={menukey} className="active">
+                                    <Link to ={menu?.path}>{menu?.name}
+                                    </Link>
+                                    {menu.child &&(
+                                        <ul className="header__menu__dropdown">
+                                            {menu.child.map((childItem, childKey) =>(
+                                                <li key={`${childItem}-${childKey}`}>
+                                                    <Link to={childItem.path}>{childItem.name}</Link>
+                                                </li>
+                                            )
 
-                
+                                            )}
+                                    
+                                        </ul>
+                                        )
+                                    }
+
+                                </li>
+                        ))},
+                    </ul>
+                </nav>
             </div>
 
         </div>
     )
 };
+
 
 export default memo(Header);
--- src/utils/router.js
+++ src/utils/router.js
@@ -2,6 +2,15 @@ export const ROUTERS = {
     USER:{
         HOME:"",
         CART: "gio-hang",
-        ABOUTUS: "ve-chung-toi"
+        WHISHLIST: "yeu-thich",
+        LOGIN: "dang-nhap",
+        ABOUTUS: "ve-chung-toi",
+        BEP: "/bep",
+        DONDEP: "/dondep",
+        TIENICH: "/tienich",
+        BLOG: "blog",
+        CONTACT: "lien-he",
+        POLICY: "chinh-sach",
+
     }
 }
